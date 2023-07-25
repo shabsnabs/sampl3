d8ab7bc1234085415ec97b81e19b68e6ec066d07
shabsnabs <shabanassl@gmail.com>
2023-07-25 12:05:35 +0530

    commit my new samplv3

--- testv3/src/main/java/testv3/TestSample3.java
+++ testv3/src/main/java/testv3/TestSample3.java
@@ -5,6 +5,7 @@ public class TestSample3 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Sample v3hi  1234");
+		System.out.println("Sample v123hi  1234");
 	}
 
 }
